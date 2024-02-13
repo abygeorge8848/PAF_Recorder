9023c0ce747249d436bab125472cdc73b006af41
Aby George <aby.george@iqvia.com>
2024-02-14 01:25:44 +0530

    Added the installation batch file

---
+++ install.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Change the next line to the path where you want to create your venv
+set VENV_DIR=%~dp0venv
+
+echo Creating virtual environment...
+python -m venv %VENV_DIR%
+
+echo Activating virtual environment...
+call %VENV_DIR%\Scripts\activate.bat
+
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+
+echo Running PyInstaller...
+call pyinstaller --onefile --noconsole your_script_name.py
+
+echo Deactivating virtual environment...
+call %VENV_DIR%\Scripts\deactivate.bat
+
+echo Done.
+pause
